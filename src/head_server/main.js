10a81bfbe5ae8491f81fdd0bb56ba074d8fd8898
Russley Shaw <russleyshaw@gmail.com>
2016-08-30 21:02:55 -0500

    Fixed cross origin stuff

--- src/head_server/main.js
+++ src/head_server/main.js
@@ -2,10 +2,18 @@ var express = require("express");
 var fs = require("fs");
 
 function main() {
+    console.log("Running Head Server...");
+
     var currentVisGame = 0;
 
     var app = express();
-    
+
+    app.use( function(req, res, next) {
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+        res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
+        next();
+    });
 
     app.get('/api/vis/next', function(req, res){
 
--- src/mock/visualizer/main.js
+++ src/mock/visualizer/main.js
@@ -6,6 +6,12 @@ function main() {
 
     var app = express();
 
+    app.use( function(req, res, next) {
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+        res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
+        next();
+    });
 
     app.get('/', function(req, res){
         res.sendFile(__dirname + "/index.html");
