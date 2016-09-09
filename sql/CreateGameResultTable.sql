70bd74a4a3c218218ec34e577ca6d0a2786d4a4c
tjk43b <tjk43b@mst.edu>
2016-09-09 15:00:16 -0500

    Update CreateGameResultTable.sql
    This should be executed after CreateClientTable.sql, but doesn't have to be.

--- sql/CreateGameResultTable.sql
+++ sql/CreateGameResultTable.sql
@@ -1,18 +1,16 @@
-CREATE TABLE "Game_Result" (
-    "Game_ID" integer PRIMARY KEY,
-    "Client" integer[],
+CREATE TABLE "game_result" (
+    "game_id" integer PRIMARY KEY,
     hashes character varying[],
-    "Winner" integer[],
-    "Reason" character varying,
-    "StartTime" timestamp with time zone,
-    "FinishTime" timestamp with time zone,
-    "GameLog" integer
+    "reason" character varying,
+    "starttime" timestamp with time zone,
+    "finishtime" timestamp with time zone,
+    "gamelog" integer
 );
 
 
-ALTER TABLE "Game_Result" OWNER TO postgres;
+--ALTER TABLE "game_result" OWNER TO postgres;
 
-CREATE SEQUENCE "Game_Result_Game_ID_seq"
+CREATE SEQUENCE "game_result_game_id_seq"
     START WITH 1
     INCREMENT BY 1
     NO MINVALUE
@@ -20,8 +18,8 @@ CREATE SEQUENCE "Game_Result_Game_ID_seq"
     CACHE 1;
 
 
-ALTER TABLE "Game_Result_Game_ID_seq" OWNER TO postgres;
+--ALTER TABLE "Game_Result_Game_ID_seq" OWNER TO postgres;
 
-ALTER SEQUENCE "Game_Result_Game_ID_seq" OWNED BY "Game_Result"."Game_ID";
+--ALTER SEQUENCE "Game_Result_Game_ID_seq" OWNED BY "Game_Result"."Game_ID";
 
-ALTER TABLE ONLY "Game_Result" ALTER COLUMN "Game_ID" SET DEFAULT nextval('"Game_Result_Game_ID_seq"'::regclass);
+--ALTER TABLE ONLY "Game_Result" ALTER COLUMN "Game_ID" SET DEFAULT nextval('"Game_Result_Game_ID_seq"'::regclass);
