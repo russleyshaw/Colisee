bc02bfb54abc0486585ee5c16e5f5811ec9b6848
gary <grwqk6@mst.edu>
2016-09-11 17:06:11 -0500

    this is my first change that includes partial status code

---
+++ src/head_server/schedulers/main.js
@@ -0,0 +1,8 @@
+/**
+ * Created by gary on 9/10/16.
+ */
+var express = require("express");
+var config = require("config");
+
+
+
---
+++ src/head_server/schedulers/scheduler.js
@@ -0,0 +1,6 @@
+/**
+ * Created by gary on 9/9/16.
+ */
+/** this needs to be an API that starts my class file */
+/** need to make a  " start" button in status.html */
+// need to have a running,paused,stopped  status
