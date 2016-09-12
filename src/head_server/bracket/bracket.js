449f07626c6a28c8970294f87c48d4911b177793
ryanloeffelman <ryanloeffelman@me.com>
2016-09-12 14:19:58 -0500

    Add the begining stages of a bracket visualizer

--- src/head_server/bracket/bracket.js
+++ src/head_server/bracket/bracket.js
@@ -1,6 +1,6 @@
-var express = require("express");
-var path = require("path");
-var router = express.Router();
+const express = require("express");
+const path = require("path");
+const router = express.Router();
 
 router.get("/bracket/", function(req, res){
     res.sendFile(path.join(__dirname, "bracket.html"));
