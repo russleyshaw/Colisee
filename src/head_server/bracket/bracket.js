86dfcad8b55ef7d8523d8fc6ceee22b7457932a3
ryanloeffelman <ryanloeffelman@me.com>
2016-09-13 15:06:52 -0500

    Add a few pages for testing both d3 and jquery bracket for web visualization

--- src/head_server/bracket/bracket.js
+++ src/head_server/bracket/bracket.js
@@ -6,4 +6,12 @@ router.get("/bracket/", function(req, res){
     res.sendFile(path.join(__dirname, "bracket.html"));
 });
 
+router.get("/bracket/d3test/", function(req, res){
+    res.sendFile(path.join(__dirname, "d3test.html"));
+});
+
+router.get("/bracket/jquerytest/", function(req, res){
+    res.sendFile(path.join(__dirname, "jquerybrackettest.html"));
+});
+
 module.exports = router;
