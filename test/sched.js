2caef8be7ad92e9f490c756a91ea1143821d9281
gary <grwqk6@mst.edu>
2016-09-19 16:40:02 -0500

    Added  start and stop functions  to Scheduler, added the mocha tests for both functions to sched.js,, added chai library for testing to package.json, took all working functions out of baseScheduler and put into Scheduler but left virtual functionality for inheritance .

--- test/sched.js
+++ test/sched.js
@@ -3,24 +3,104 @@
  */
 /* eslint-env node, mocha */
 var should = require("should");
+var assert = require("assert");
+var chai = require('chai'),
+    show=chai.should;
 var RandomScheduler = require("../src/head_server/schedulers/RandomSchedulerType");
-//var Scheduler = require("../src/head_server/schedulers/BaseSchedulerType");
-
+var BaseScheduler = require("../src/head_server/schedulers/BaseSchedulerType");
+var Scheduler = require("../src/head_server/schedulers/Scheduler");
 //
+describe("Scheduler", function() {
+    describe("RandomSchedulerType", function () {
+        describe("start", function () {
+            it("Should not start this scheduler.", function (done) {
+                var d = new Scheduler(20,1000);
+                should(d.num_scheduled()).be.equal(0);
+                done();
+            });
+            it("Should start this scheduler.", function (done) {
+                var d = new Scheduler(20,1000);
+                d.start();
+                should(d.num_scheduled()).be.equal(20);
+                done();
+            });
+        });
+    });
+});
+describe("Scheduler", function() {
+    describe("RandomSchedulerType", function () {
+        describe("stop", function () {
+            it("Should not stop this scheduler.", function (done) {
+                var d = new Scheduler(20,1000);
+                d.start();
+                should(d.num_scheduled()).be.equal(20);
+                done();
+            });
+            it("Should set is_started = false", function (done) {
+                var d = new Scheduler(2000000,3000);
+                var a, b, c;
+                a = setInterval(function(){ d.stop(); }, 500);
+
+                //d.stop();
+                b = setInterval(function() { should.equal(d.is_started ,false); }, 1000);
+                c = setInterval(function() { done(); }, 1050);
+
+                //0 ms
+                d.start();
+
+                setInterval(function() {
+                    clearInterval(a);
+                    clearInterval(b);
+                    clearInterval(c);
+
+                }, 1051);
+
+                //1 ms
+
+
+            });
+            it("Should purge match_queue.", function (done) {
+                var d = new Scheduler(2000000,3000);
+                var a, b, c;
+                a = setInterval(function () {
+                    d.stop();
+                }, 500);
+
+                //d.stop();
+                b = setInterval(function () {
+                    should.equal(d.is_started, false);
+                }, 1000);
+                c = setInterval(function () {
+                    done();
+                }, 1050);
+
+                //0 ms
+                d.start();
+
+                setInterval(function () {
+                    clearInterval(a);
+                    clearInterval(b);
+                    clearInterval(c);
+
+                }, 1051);
+            });
+
+        });
+    });
+});
 // describe("Scheduler", function() {
 //     describe("RandomSchedulerType", function () {
-//         describe("start", function () {
-//             it("Should not start this scheduler.", function (done) {
-//                 var d = new RandomScheduler(20,1000);
-//                 should(d.num_scheduled()).be.equal(0);
-//                 done();
-//             });
-//             it("Should start this scheduler.", function (done) {
-//                 var d = new RandomScheduler(20,1000);
+//         describe("pause", function () {
+//             it("Should pause this scheduler and have matches in queue.", function (done) {
+//                 var d = new Scheduler(20000,3000);
+//                 setInterval(function(){d.pause() }, 500);
 //                 d.start();
+//                 //d.pause();
+//                 should.equal(d.is_started ,false);
 //                 should(d.num_scheduled()).be.equal(20);
 //                 done();
 //             });
+//
 //         });
 //     });
 // });
@@ -35,7 +115,7 @@ var RandomScheduler = require("../src/head_server/schedulers/RandomSchedulerType
 //             });
 //             it("should return 1 when a games have been scheduled",function(done){
 //                 var d = new Scheduler(20,1000);
-//                 d.switch_to( new RandomScheduler() );
+//                 d.switch_to(new RandomScheduler() );
 //                 d.schedule_once();
 //                 should(d.num_scheduled()).be.equal(1);
 //                 done();
@@ -48,16 +128,24 @@ describe("Scheduler", function(){
     describe("RandomSchedulerType", function(){
         describe("schedule_once()", function(){
             it("should return 0 when no game has been scheduled",function(done){
-                var d = new RandomScheduler(20,1000);
+                var d = new Scheduler(20,1000);
                 should(d.num_scheduled()).be.equal(0);
                 done();
             });
             it("should return 1 when a game has been scheduled",function(done){
-                var d = new RandomScheduler(20,1000);
+                var d = new Scheduler(20,1000);
                 d.schedule_once();
                 should(d.num_scheduled()).be.equal(1);
                 done();
             });
+            it("should return 2 when .schedule_once() has been selected twice",function(done){
+                var d = new Scheduler(20,1000);
+                d.schedule_once();
+                d.schedule_once();
+                should(d.num_scheduled()).be.equal(2);
+                done();
+            });
+
 
         });
 
@@ -66,24 +154,35 @@ describe("Scheduler", function(){
 // describe("Scheduler", function(){
 //     describe("RandomSchedulerType", function(){
 //         describe("next()", function(){
-//             it("should return 1 when a game has been scheduled",function(done){
-//                 var d = new Scheduler();
+//             it("should return undefined when a match has not been scheduled", function(done) {
+//                 var d = new Scheduler(20,1000);
 //                 d.next();
-//                 should(d.next).be.ok()
+//                 //assert.equal(d.next(), undefined);
+//                 should.equal(d.next(),null);
+//                 done();
+//             });
+//             it("should return a match when 1 match has been scheduled",function(done){
+//                 var d = new Scheduler(20,1000);
+//                 d.schedule_once();
+//                 d.next();
+//                 //console.log(d.next());
+//                 should.notEqual(d.next(),null);
+//
+//
+//                 done();
+//             });
+//             it("should return next match when 20 matches have been scheduled",function(done){
+//                 var c = new Scheduler(20,1000);
+//                 //d.schedule_once();
+//                 c.start();
+//                 //console.log(d.next());
+//                 should.notEqual(c.next(),null);
+//
+//
 //                 done();
 //             });
 //         });
 //
 //     });
 // });
-// describe("create", function() {
-//     it("should create a new client in the database", function (done) {
-//         Scheduler.create("user1", "repo1", "hash1", "cpp", function (err, client) {
-//             should(err).not.be.ok();
 //
-//             done();
-//         });
-//
-//     });
-// });
-
