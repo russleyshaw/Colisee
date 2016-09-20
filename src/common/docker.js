d3e7f44fe4892c46d201cc560db90213319b910c
Russley Shaw <russleyshaw@gmail.com>
2016-09-20 00:35:34 -0500

    Working test db, logger, new client tests

---
+++ src/common/docker.js
@@ -0,0 +1,24 @@
+/* eslint-disable */
+//TODO: Enable eslint
+
+//TODO: Expand common docker interface
+
+class Docker {
+
+    /**
+     * Builds a docker image
+     * @param image_tag
+     * @param dockerfile
+     * @param callback
+     */
+    build(image_tag, dockerfile, callback) {
+    }
+
+    run(container_tag, image_tag, callback) {
+    }
+
+    save(image_tag, filename) {
+    }
+
+
+}
