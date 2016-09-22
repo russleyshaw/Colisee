3856043f49b8972d34ad82e74b73d61663b31724
Russley Shaw <russleyshaw@gmail.com>
2016-09-22 10:20:49 -0500

    Update builder.js

--- test/integration/builder.js
+++ test/integration/builder.js
@@ -12,7 +12,7 @@ describe("Builder", function() {
         Db.reset(function(err){
             should(err).not.be.ok();
             Db.queryLots([
-                [ "INSERT INTO client (name, git_repo, git_hash, language) VALUES ($1::text, $2::text, $3::text, $4) RETURNING *", ["test1", "https://github.com/russleyshaw/Joueur.cpp.git", "98ae5ac0daa867a7ec98f2f5f8f2add6dc91c00c", "cpp"]]
+                [ "INSERT INTO client (name, git_repo, git_hash, language) VALUES ($1::text, $2::text, $3::text, $4) RETURNING *", ["test1", "https://github.com/russleyshaw/Joueur.cpp.git", "ca3803e2a120668b1715ba29cafbad3fed4b10ce", "cpp"]]
             ], function(err, results) {
                 should(err).not.be.ok();
 
