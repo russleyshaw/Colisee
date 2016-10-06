3a4efe55e6d2620db6ef6acc147f1bf4d4c1a307
Tyler Kaufman <tjk43b@mst.edu>
2016-10-06 15:24:03 -0500

    Updated Logger
    Updated the logger and added a basic get_latest() function which gets the latest number of logs specified, and they are ordered by created_time.
    Will continue to work on this (or a similar) function to add functionality for specifiying certain severity levels.

--- src/common/logger.js
+++ src/common/logger.js
@@ -1,73 +1,90 @@
 var Db = require("./Db");
+var knex = require("knex")({
+    dialect: "pg"
+});
 
+/**
+ * Class to interface with log table in database
+ */
 class Logger {
 
-    /**
-     * Gets a single log from the database
-     * @param log_id
-     * @param callback function(err, log)
-     */
-    static get(log_id, callback) {
-
-        var pgclient = Db.newPgClient();
-
-        pgclient.connect(function(err) {
+    static getById(log_id, callback) {
+        var sql = knex("log").where("id", log_id).toString();
+        Db.queryOnce(sql, [], function (err, result) {
             if(err) return callback(err);
+            if(result.rows.length != 1) return callback("No match found");
 
-            pgclient.query("SELECT * FROM log WHERE id = $1::integer", [log_id], function(err, result) {
-                if(err) return callback(err);
-                if(result.rowCount != 1) return callback("No match found");
-
-                var data = {
-                    id: result.rows[0].id,
-                    message: result.rows[0].message,
-                    location: result.rows[0].location,
-                    severity: result.rows[0].severity,
-                    time_created: result.rows[0].time_created
-                };
-
-                pgclient.end(function (err) {
-                    if(err) return callback(err);
-                    callback(null, data);
-                });
-            });
+            callback(null, result.rows[0]);
         });
     }
 
     /**
-     * Create a log with the given parameters
-     * @param message
-     * @param location
-     * @param severity
-     * @param callback - function(err, created_log)
+     * @callback Logger~createCallback
+     * @param err
+     * @param log {Object} newly created log
      */
-    static create(message, location, severity, callback) {
-        var pgclient = Db.newPgClient();
 
-        pgclient.connect(function(err){
+    /**
+     * Creates a log from the given object
+     * @param log {Object}
+     * @param callback {Logger~createCallback}
+     */
+    static create(log, callback) {
+        if(log.hasOwnProperty("id")) return callback("log ids are created automatically");
+        if(log.hasOwnProperty("created_time")) return callback("Cannot create with created time");
+        if(log.hasOwnProperty("modified_time")) return callback("Cannot create with modified time");
+
+        log.created_time = "now()";
+        log.modified_time = "now()";
+
+        var sql = knex("log").insert(log, "*").toString();
+        Db.queryOnce(sql, [], function(err, result) {
             if(err) return callback(err);
-
-            pgclient.query("INSERT INTO log (message, location, severity, created_time) VALUES ($1::text, $2::text, $3, now() ) RETURNING *", [message, location, severity], function (err, result) {
-                if(err) return callback(err);
-                if(result.rowCount != 1) { callback("No match found"); return; }
-
-                var data = {
-                    id: result.rows[0].id,
-                    message: result.rows[0].message,
-                    location: result.rows[0].location,
-                    severity: result.rows[0].severity,
-                    created_time: result.rows[0].created_time
-                };
-
-                pgclient.end(function(err){
-                    if(err) return callback(err);
-                    callback(null, data);
-                });
-            });
+            if(result.rowCount != 1) return callback("Inserted row not returned");
+            callback(null, result.rows[0]);
         });
     }
-    static latest() {
-        return 1;
+
+    /**
+     * @callback Logger~updateByIdCallback
+     * @param err
+     * @param log {Object} Updated log
+     */
+
+    /**
+     * Update log given the id and fields to update
+     * @param id {number} Logger database id
+     * @param fields {Object} JSON of fields to update
+     * @param callback {Logger~updateByIdCallback}
+     */
+    static updateById(id, fields, callback) {
+        if(fields.hasOwnProperty("id")) return callback("Cannot update id");
+        if(fields.hasOwnProperty("created_time")) return callback("Cannot update created time");
+        if(fields.hasOwnProperty("modified_time")) return callback("Cannot update modified time");
+
+        fields["modified_time"] = "now()";
+
+        var sql = knex("log").where({id: id}).update(fields, "*").toString();
+        Db.queryOnce(sql, [], (err, result) => {
+            if(err) return callback(err);
+            if(result.rows.length != 1) return callback("Query resulted in invalid number of rows");
+
+            callback(null, result.rows[0]);
+        });
+    }
+
+    /**
+     * Retrieve <code>limit</code> latest number of logs
+     * @param limit {number} Number of latest logs to select
+     * @param callback
+     */
+    static get_latest(limit, callback) {
+        var sql = knex("log").orderBy("created_time").limit(limit).toString();
+        Db.queryOnce(sql, [], function(err, result) {
+            if(err) return callback(err);
+            if(result.rowCount != limit) return callback("Inserted rows not fully returned");
+            callback(null, result.rows);
+        });
         //TODO: latest() should get the 10 latest logs, based on creation timeim ba
     }
 }
