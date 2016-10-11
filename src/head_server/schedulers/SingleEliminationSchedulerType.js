cfa9eed3f00cf5bc6be643064b5f27f52c94c8a3
gary warrren <grwqk6@mst.edu>
2016-10-11 08:59:30 -0500

    Created SingleELiminationSchedulerType.

---
+++ src/head_server/schedulers/SingleEliminationSchedulerType.js
@@ -0,0 +1,20 @@
+var BaseScheduler = require("./BaseSchedulerType");
+//var Db = require("../../common/Db");
+//var Client = require("../../common/Client");
+// var knex = require("knex")({
+//     dialect: "pg"
+// });
+
+
+class SingleEliminationSchedulerType extends BaseScheduler {
+    constructor() {
+        super();
+    }
+
+}
+
+
+
+
+
+module.exports = SingleEliminationSchedulerType;
