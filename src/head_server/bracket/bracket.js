93e9e34332c26f02db3718d6fe1c47f504928beb
Quan Gip <qgdgf@mst.edu>
2016-10-11 11:39:23 -0500

    Add Hyperlink and clean up the code/branch

--- src/head_server/bracket/bracket.js
+++ src/head_server/bracket/bracket.js
@@ -6,16 +6,4 @@ router.get("/bracket/", function(req, res){
     res.sendFile(path.join(__dirname, "bracket.html"));
 });
 
-router.get("/bracket/d3test/", function(req, res){
-    res.sendFile(path.join(__dirname, "d3test.html"));
-});
-
-router.get("/bracket/jquerytest/", function(req, res){
-    res.sendFile(path.join(__dirname, "jquerybrackettest.html"));
-});
-
-router.get("/bracket/testData/", function(req, res){
-    res.sendFile(path.join(__dirname, "testData.json"));
-});
-
 module.exports = router;
