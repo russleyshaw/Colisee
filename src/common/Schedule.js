ec642fe1007b0208d162ebbf1b31f1ec6f9cda5b
gary warrren <grwqk6@mst.edu>
2016-10-19 21:46:26 -0500

    update comments

--- src/common/Schedule.js
+++ src/common/Schedule.js
@@ -15,8 +15,8 @@ class Schedule{
 
     /**
      * Creates a schedule when given a scheduler type
-     * @param scheduler type
-     * @param callback {Schedule~createCallback}
+     * @param schedule {Object}
+     * @callback {Schedule~createCallback}
      */
     static create(schedule, callback){
         if(schedule.hasOwnProperty("id")) return callback("schedule ids are created automatically");
@@ -33,6 +33,17 @@ class Schedule{
             callback(null,result.rows[0]);
         });
     }
+
+    /**
+     * @callback
+     * @param err
+     * @param Schedule {Object}
+     *
+     *
+     * Retreives a schedule by its ID#
+     * @param a schedule id
+     * @callback {Schedule ~getByIDcallback}
+     */
     static getByID(schedule_id, callback){
         var sql = knex("schedule").where("id",schedule_id).toString();
         Db.queryOnce(sql,[],function(err,result){
@@ -40,6 +51,17 @@ class Schedule{
             callback(null,result.rows[0]);
         });
     }
+
+    /**
+     * @callback
+     * @param err
+     * @param Schedule {Object}
+     *
+     *
+     *
+     * @param a schedule type
+     * @callback {Schedule ~getByTypeCallback}
+     */
     static getByType(schedule_type, callback){
         var sql = knex("schedule").where("type",schedule_type).toString();
         Db.queryOnce(sql,[],function(err,result){
