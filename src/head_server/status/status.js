49cf8f68cd48147ddf045d7fa23573e77aea955b
Unknown <russleyshaw@gmail.com>
2016-11-27 11:13:16 -0600

    Moved from src dir

--- src/head_server/status/status.js
+++
@@ -1,9 +0,0 @@
-var express = require("express");
-var path = require("path");
-var router = express.Router();
-
-router.get("/status/", function(req, res){
-    res.sendFile(path.join(__dirname, "status.html"));
-});
-
-module.exports = router;
